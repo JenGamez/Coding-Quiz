3048bb7ea25e3a6a788c83e55f7caa883994d9db
Jennifer Gamez <jenggamez@gmail.com>
2019-10-12 08:21:20 -0700

    Coding Quiz

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Coding-Quiz
-10 question timed coding quiz
+# jengamez.github.io
+# Unit 02: CSS and Bootstrap Homework: Resonsive Portfolio
+
+## Description
+This is a 60 second timed quiz on coding. I began with the simplest task, making the questions.  I went through many different ways to syntax the questions, choices and answers. In the end, I decided to use the index. 
+
+I created a function for start time. Within this a setInterval () that will repeatly call a function at 1000 milliseconds.  The clearInterval () is called to stop the timer when seconds equal zero.
+
+I made an Event listener called the startTime () to begin the timer.  Next the hideStartArea () is called to hide the <h1>, <p>, and <button> elements. Then the displayQuestion () is called to display the first question.  
+
+When the user clicks on one of the choices, a series of actions happens. The srcElement event references which choice was clicked on, and given the variable clickID. An answer variable was created so I can see in the console log which choice was selected.  correctID variable was created to compare to the correctAnswer. If they match, then the score increments.
+
+In the dislayQuestion () I made the h1 the question.  So that the questions wouldn't show up on the page over and over, I cleared the wrapper's contents using the innerHTML property with empty quotes.  The question element was appended to the parent wrapper element. I made a div for the choices element, so that I could append the buttonEl div to it. I made a for loop to loop through the questions and choices.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ questions.js
@@ -0,0 +1,145 @@
+var myQuestions = [
+
+    // The below array containing object literal, which is a comma-seperated list of name-value pairs
+
+    // First question. 
+    {
+        question: "What symbol comes after a function?",
+        choices: [
+            ";",
+            "()",
+            "{}",
+            "[]",
+        ],
+
+        correctAnswer: 1
+    },
+
+    // Second question 
+    {
+        question: "Which function converts an argument to a string and returns a floating point number?",
+        choices: [
+            "parseFloat()",
+            "toFixed()",
+            "valueOf()",
+            "toString()",],
+
+        correctAnswer: 0
+    },
+    // Third question 
+    {
+        question: "Which method combines the text of two strings and turns them into one string?",
+        choices: ["concat()",
+            "charAt()",
+            "length()",
+            "match()",
+        ],
+        correctAnswer: 0
+    },
+
+
+
+
+    // Fourth question 
+    {
+        question: "Which symbol means equal value and equal type?",
+        choices: [
+            "=",
+
+            "==",
+
+            "===",
+
+            "!=",
+        ],
+
+        correctAnswer: "2"
+
+    },
+
+    // Fifth question
+    {
+        question: "Which method formats a number with a specific number of digits to the right of the decimal?",
+        choices: [
+            "toPrecision",
+            "valueOf",
+            "toExponential",
+            "toFixed",
+        ],
+
+        correctAnswer: "3"
+    },
+
+    // Sixth question
+    {
+        question: "Which symbol means 'or'?",
+        choices: [
+            "&",
+
+            "|",
+
+            "||",
+
+            "~",
+        ],
+
+        correctAnswer: "2"
+    },
+
+    // Seventh question
+    {
+        question: "Which function determines whether a value is an illegal number?",
+        choices: [
+            "parseInt",
+            "isNan",
+            "decodeURI",
+            "isFinite",
+        ],
+
+        correctAnswer: "1"
+    },
+
+    // Eigth question
+    {
+        question: "All properties have a name and a what?",
+        choices: [
+            "attribute",
+
+            "value",
+
+            "element",
+            "function",
+        ],
+
+        correctAnswer: "1"
+    },
+
+    // Ninth question
+    {
+        question: "Where is the the optimal location to place the linking <script> tag in an HTML document?",
+        choices: [
+            "right after </body>",
+            "right before </body>",
+            "right before </head>",
+            "right after </head>",
+        ],
+
+        correctAnswer: "1"
+    },
+
+    // Tenth question
+    {
+        question: "Which one is not a JavaScript datatype?",
+        choices: [
+            "Symbols",
+
+            "Numbers",
+
+            "Booleans",
+
+            "strings",
+        ],
+
+        correctAnswer: "0"
+    }
+];
